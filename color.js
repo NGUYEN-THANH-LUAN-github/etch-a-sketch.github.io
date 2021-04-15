a0d23cb70491723e9d11229cfe069523dd26591d
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-04-15 13:26:18 +0900

    few modifications

---
+++ color.js
@@ -0,0 +1,5 @@
+const black = document.querySelector(".black");
+const rainbow = document.querySelector(".rainbow");
+const color = document.querySelector(".color");
+
+black =
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const randNum = (min, max) => (Math.floor(Math.random() * (max - min + 1)) + min);
 const pad = document.querySelector(".pad ");
-const set = document.querySelector(".set");
+const toggleGrid = document.querySelector(".toggle-grid");
 const reset = document.querySelector(".reset");
 
 // slider
@@ -14,28 +14,33 @@ input.addEventListener("input", () => {
 
 setGrid(input.value);
 
-set.addEventListener("click", () => {
+input.addEventListener("change", () => {
     while (pad.lastElementChild) {
         pad.removeChild(pad.lastElementChild);
     }
     setGrid(input.value);
 })
 
+toggleGrid.addEventListener('click', gridToggle);
+
 reset.addEventListener("click", eraseAll);
+
+let isDrawing = false;
+window.addEventListener('mousedown', () => {
+    isDrawing = true;
+})
+window.addEventListener('mouseup', () => {
+    isDrawing = false;
+})
 
 function setGrid(grid) {
     pad.style.cssText = `grid-template: repeat(${grid}, 1fr) / repeat(${grid}, 1fr)`;
-    let isDrawing = false;
     for (let i = 1; i <= grid * grid; i++) {
         const div = document.createElement('div');
         div.classList.add('grid');
+        div.classList.add('grid-border');
         pad.appendChild(div);
-        div.addEventListener('mousedown', () => {
-            isDrawing = true;
-        })
-        div.addEventListener('mouseup', () => {
-            isDrawing = false;
-        })
+
         div.addEventListener('mouseover', () => {
             if (isDrawing) {
                 const r = randNum(0, 255);
@@ -47,9 +52,18 @@ function setGrid(grid) {
     }
 }
 
+
+
 function eraseAll() {
     const divs = pad.querySelectorAll("div");
     for (let div of divs) {
         div.style.backgroundColor = "white";
+    }
+}
+
+function gridToggle() {
+    const divs = pad.querySelectorAll("div");
+    for (let div of divs) {
+        div.classList.toggle('grid-border');
     }
 }
