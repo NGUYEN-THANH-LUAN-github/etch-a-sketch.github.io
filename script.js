9195422022d15967492dd088c9620911bd254245
NGUYEN THANH LUAN <luanconsutorakushon@gmail.com>
2021-04-15 22:26:17 +0900

    project completed, code needs refactoring later

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const pad = document.querySelector(".pad ");
 const toggleGrid = document.querySelector(".toggle-grid-gap");
 const reset = document.querySelector(".reset");
 const button2s = document.querySelectorAll(".button2");
+const input = document.querySelector("input");
+const outputs = document.querySelectorAll("output");
+const blackColor = document.querySelector(".black");
+const rainbowColor = document.querySelector(".rainbow");
+const picker = document.querySelector(".picker");
+const eraser = document.querySelector(".eraser");
+const customColor = document.querySelector(".picked-color");
+const backColor = document.querySelector(".background-color");
+const backColorPicker = document.querySelector(".background-color-picker");
+const span = document.querySelector("span");
 
 for (let button2 of button2s) {
     button2.addEventListener("click", () => {
@@ -14,8 +24,7 @@ for (let button2 of button2s) {
 }
 
 // slider
-const input = document.querySelector("input");
-const outputs = document.querySelectorAll("output");
+
 input.addEventListener("input", () => {
     for (let output of outputs) {
         output.innerText = input.value;
@@ -38,6 +47,8 @@ toggleGrid.addEventListener('click', () => {
 
 reset.addEventListener("click", eraseAll);
 
+
+
 let isDrawing = false;
 
 window.addEventListener('mousedown', () => {
@@ -49,11 +60,7 @@ window.addEventListener('mouseup', () => {
 
 
 let color = "rainbow";
-const blackColor = document.querySelector(".black");
-const rainbowColor = document.querySelector(".rainbow");
-const picker = document.querySelector(".picker");
-const eraser = document.querySelector(".eraser");
-const customColor = document.querySelector(".picked-color");
+
 
 blackColor.addEventListener("click", () => {
     color = "black";
@@ -68,11 +75,15 @@ eraser.addEventListener("click", () => {
     color = "eraser";
 })
 
+
+
 function setGrid(grid) {
     pad.style.cssText = `grid-template: repeat(${grid}, 1fr) / repeat(${grid}, 1fr)`;
+    let bC = backColor.value;
     for (let i = 1; i <= grid * grid; i++) {
         const div = document.createElement('div');
-        div.classList.add('grid');
+        // div.classList.add('grid');
+        div.style.backgroundColor = bC;
         pad.appendChild(div);
         div.addEventListener('mouseover', () => {
             if (isDrawing) chooseBrush(div)
@@ -113,16 +124,49 @@ function custom(div) {
 }
 
 function erase(div) {
-    div.style.backgroundColor = "white";
+    div.style.backgroundColor = backColor.value;
 }
 
 function eraseAll() {
     const divs = pad.querySelectorAll("div");
     for (let div of divs) {
-        div.style.backgroundColor = "white";
+        div.style.backgroundColor = backColor.value;
+    }
+}
+
+backColor.addEventListener("change", () => {
+    span.innerText = "Click to change";
+    const { r, g, b } = hexToRgb(backColor.value);
+    if ((r + g + b) < 383) {
+        span.style.color = "white";
+    } else {
+        span.style.color = "black";
+    }
+    backColorPicker.style.backgroundColor = backColor.value;
+})
+
+backColorPicker.addEventListener("click", changeBackColor);
+
+function changeBackColor() {
+    const divs = pad.querySelectorAll("div");
+    backColorPicker.style.backgroundColor = "";
+    span.innerText = "BG Color: ";
+    span.style.fontSize = "inherit";
+    span.style.color = "inherit";
+    for (let div of divs) {
+        div.style.backgroundColor = backColor.value;
     }
 }
 
 function gridToggle() {
     pad.classList.toggle('grid-gap');
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
 }
